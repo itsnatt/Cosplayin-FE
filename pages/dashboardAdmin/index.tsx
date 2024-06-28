9e92f247ec3270a304f3831a4bb8fcd934378d92
dwkurnie <kurniawan7996@gmail.com>
2024-06-28 16:06:27 +0700

    update minor

--- pages/dashboardAdmin/index.tsx
+++ pages/dashboardAdmin/index.tsx
@@ -1,17 +1,4 @@
-import React from 'react';
 import Auth from 'src/auth/Auth';
-
-// const DashboardAdmin: React.FC = () => {
-//   return (
-//     <div>
-//       <h1>Dashboard</h1>
-//       <p>Welcome to your dashboard!</p>
-//     </div>
-//   );
-// };
-
-// export default Auth(DashboardAdmin);
-// pages/Register.tsx
 
 import DashboardAdmin from "containers/DashboardAdmin/DashboardAdmin";
 
