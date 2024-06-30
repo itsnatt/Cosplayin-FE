385260a6930f57d75486012addb2c6cbbe8d1d66
Yuuna <92516142+argiekurniawan@users.noreply.github.com>
2024-07-01 00:17:59 +0700

    image di about

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -20,6 +20,7 @@
 
 // pages/about.js
 
+import React from 'react';
 
 import NavBar from 'components/Layout/Header/Navbar/Navbar';
 import Link from 'next/link';
@@ -29,7 +30,7 @@ const About = () =>  {
     return (
         <div>
           <NavBar />
-            <section className="bg-white dark:bg-blue-500" style={{ backgroundImage: "url('/img-use/BGCosplayIn.JPG')" }}>
+            <section className="bg-white dark:bg-blue-500" style={{ backgroundImage: "url('/image/BGCosplayIn.JPG')" }}>
                 <div className="container mx-auto px-6 py-16 text-center flex flex-col justify-center items-center h-screen">
                     <div className="max-w-lg">
                         <h1 className="text-4xl font-bold text-gray-800 dark:text-white lg:text-4xl">Tempat Mencari Costume Cosplay Pertama Di Indonesia!</h1>
@@ -55,39 +56,30 @@ const About = () =>  {
                             </div>
                             <div className="w-full md:w-1/2">
                                 <div className="block">
-                                    {/* <img src="/img-use/Photo1.JPG" className="object-cover rounded-lg max-h-64 sm:max-h-96 btn- w-full h-full" /> */}
+                                    {<img src="/image/Photo1.JPG" className="object-cover rounded-lg max-h-64 sm:max-h-96 btn- w-full h-full" />}
                                 </div>
                             </div>
                         </div>
                         <div className="grid grid-cols-12 sm:px-5 gap-x-8 gap-y-16">
                             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
-                                {/* <img src="/img-use/Photo2.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" /> */}
+                                {<img src="/image/Photo2.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" />}
                                 <a className="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
                                 <p className="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                                 <div className="pt-2 pr-0 pb-0 pl-0">
-                                    <a className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
-                                    <p className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
-                                    <p className="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
                                 </div>
                             </div>
                             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
-                                {/* <img src="/img-use/Photo6.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" /> */}
+                                {<img src="/image/Photo6.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" />}
                                 <a className="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
                                 <p className="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                                 <div className="pt-2 pr-0 pb-0 pl-0">
-                                    <a className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
-                                    <p className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
-                                    <p className="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
                                 </div>
                             </div>
                             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
-                                {/* <img src="/img-use/Photo4.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" /> */}
+                                {<img src="/image/Photo4.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" />}
                                 <a className="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
                                 <p className="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                                 <div className="pt-2 pr-0 pb-0 pl-0">
-                                    <a className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
-                                    <p className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
-                                    <p className="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
                                 </div>
                             </div>
                         </div>
@@ -101,7 +93,7 @@ const About = () =>  {
                         <div className="flex flex-col items-center sm:px-5 md:flex-row">
                             <div className="w-full md:w-1/2">
                                 <div className="block">
-                                    {/* <img src="/img-use/event1.JPG" className="object-cover rounded-lg max-h-64 sm:max-h-96 btn- w-full h-full" /> */}
+                                    {<img src="/image/event1.jpg" className="object-cover rounded-lg max-h-64 sm:max-h-96 btn- w-full h-full" />}
                                 </div>
                             </div>
                             <div className="flex flex-col items-start justify-center w-full h-full pt-6 pr-0 pb-6 pl-0 mb-6 md:mb-0 md:w-1/2">
@@ -113,33 +105,24 @@ const About = () =>  {
                         </div>
                         <div className="grid grid-cols-12 sm:px-5 gap-x-8 gap-y-16">
                             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
-                                {/* <img src="/img-use/event2.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" /> */}
+                                {<img src="/image/event2.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" />}
                                 <a className="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
                                 <p className="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                                 <div className="pt-2 pr-0 pb-0 pl-0">
-                                    <a className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
-                                    <p className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
-                                    <p className="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
                                 </div>
                             </div>
                             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
-                                {/* <img src="/img-use/event3.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" /> */}
+                                {<img src="/image/event3.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" />}
                                 <a className="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
                                 <p className="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                                 <div className="pt-2 pr-0 pb-0 pl-0">
-                                    <a className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
-                                    <p className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
-                                    <p className="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
                                 </div>
                             </div>
                             <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
-                                {/* <img src="/img-use/event4.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" /> */}
+                                {<img src="/image/event4.JPG" className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-" />}
                                 <a className="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
                                 <p className="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                                 <div className="pt-2 pr-0 pb-0 pl-0">
-                                    <a className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
-                                    <p className="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
-                                    <p className="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
                                 </div>
                             </div>
                         </div>
@@ -147,21 +130,7 @@ const About = () =>  {
                 </div>
             </section>
 
-            <section className="footer w-full text-center py-8 bg-gray-200">
-                <p className="mb-4">Disini kami memaksa anda untuk menyewa di tempat cosplay.in. karena ditempat kami adalah tempat rental terbaik didunia ajg<br />dah kubilang disini aja kalo nyewa Ngeyell, dah dah dah nanti sewa disini aja yaa, ga mau tau</p>
-                <div className="flex justify-center mb-6">
-                    <Link href="https://discord.gg/h65vrTdZcZ" legacyBehavior passHref>
-                        <a target="_blank"><FaDiscord className="mx-2 text-red-500" /></a>
-                    </Link>
-                    <Link href="https://twitter.com/kobokanaeru" legacyBehavior passHref>
-                        <a target="_blank"><FaTwitter className="mx-2 text-red-500" /></a>
-                    </Link>
-                    <Link href="https://www.instagram.com/animexepriwer/" legacyBehavior passHref>
-                        <a target="_blank"><FaInstagram className="mx-2 text-red-500" /></a>
-                    </Link>
-                </div>
-                <p className="mb-2">Made with <i className="fa fa-heart-o text-red-500"></i> by Cosplay.In</p>
-            </section>
+            
         </div>
     );
 }
